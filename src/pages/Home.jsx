afd1486502440b96b17085063daf571a9ec45cfa
Pritam <pritambose040@gmail.com>
2024-05-26 16:32:08 +0530

    minor changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect, useState } from "react";
 import appwriteService from "../appwrite/config";
 import { Container, PostCard } from "../components";
+import { useSelector } from "react-redux";
 
 function Home() {
+  const userStatus = useSelector((state) => state.auth.status);
+  const userName = useSelector((state) => state.auth.userData.name);
+  // console.log(userStatus);
   const [posts, setPosts] = useState([]);
   useEffect(() => {
     appwriteService.getPosts().then((posts) => {
@@ -19,7 +23,7 @@ function Home() {
           <div className="flex flex-wrap">
             <div className="p-2 w-full">
               <h1 className="text-2xl font-bold hover:text-gray-500">
-                Login to read posts
+                {userStatus ? `Welcome ${userName}` : "Login to read posts"}
               </h1>
             </div>
           </div>
@@ -28,14 +32,23 @@ function Home() {
     );
   }
   return (
-    <div className="w-full py-8">
+    <div className="w-full py-5 flex flex-col items-center gap-5">
+      <h1 className="text-2xl font-bold hover:text-gray-500">
+        {userStatus ? `Welcome ${userName}` : "Login to read posts"}
+      </h1>
       <Container>
         <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard {...post} />
-            </div>
-          ))}
+          {userStatus ? (
+            posts.map((post) => (
+              <div key={post.$id} className="p-2 w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))
+          ) : (
+            <h1 className="text-2xl font-bold hover:text-gray-500 mx-auto">
+              Login to read posts
+            </h1>
+          )}
         </div>
       </Container>
     </div>
