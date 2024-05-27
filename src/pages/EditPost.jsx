af9e15ed1ab7af1fec8ef85ada79897dd3fd4434
Pritam <pritambose040@gmail.com>
2024-05-28 00:37:00 +0530

    minor changes

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -5,6 +5,7 @@ import { useNavigate, useParams } from "react-router-dom";
 
 function EditPost() {
   const [post, setPost] = useState(null);
+  const [loader, setLoader] = useState(true);
   const { slug } = useParams();
   const navigate = useNavigate();
   useEffect(() => {
@@ -19,13 +20,19 @@ function EditPost() {
     }
   }, [slug, navigate]);
 
-  return post ? (
-    <div className="py-8 w-full min-h-[80vh] text-center flex-col items-center justify-center bg-bgLight text-textColor">
-      <Container>
-        <PostForm post={post} />
-      </Container>
-    </div>
-  ) : null;
+  return !loader ? (
+    <>
+      {post && (
+        <div className="py-8 w-full min-h-[80vh] text-center flex-col items-center justify-center bg-bgLight text-textColor">
+          <Container>
+            <PostForm post={post} />
+          </Container>
+        </div>
+      )}
+    </>
+  ) : (
+    <div className="text-xl p-10 font-semibold h-[80vh]">Loading...</div>
+  );
 }
 
 export default EditPost;
